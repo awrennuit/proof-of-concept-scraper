6e90889e049a8036777689354327ffa31d1d4878
AwrenNuit <a.rennaker@yahoo.com>
2020-05-11 23:56:55 -0500

    removed regex

--- scraper.js
+++ scraper.js
@@ -3,6 +3,10 @@ const cheerio = require(`cheerio`);
 
 const listUrl = `https://en.wikipedia.org/wiki/The_World's_Billionaires`;
 
+let forbesList = [];
+let twitterUrls = [];
+let wikipediaUrls = [];
+
 const customHeaderRequest = request.defaults({
   headers: {'User-Agent': `Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0`}
 });
@@ -12,9 +16,12 @@ customHeaderRequest.get(listUrl, (error, response, body) => {
       console.log('in there');
       const $ = cheerio.load(body);
       $(`.sortable`).each((i, el) => {
+        if(2020 - i === 1999) {
+          return;
+        }
         console.log(2020-i);
         const info = $(el).find(`td`);
-        console.log(info.text().replace(/\s\s+/g, ''));
+        console.log(info.text());
       });
     }
 });
