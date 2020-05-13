a8bfacad9cacf9400c1991616ae123cf00daa06f
AwrenNuit <a.rennaker@yahoo.com>
2020-05-13 11:34:02 -0500

    rough draft of data storage

--- scraper.js
+++ scraper.js
@@ -7,31 +7,41 @@ const customHeaderRequest = request.defaults({
   headers: {'User-Agent': `Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0`}
 });
 
+let topBillionairesPerYear = [];
+
 customHeaderRequest.get(listUrl, (error, response, body) => {
   if(!error && response.statusCode === 200) {
     const $ = cheerio.load(body);
     let year = 2020;
+    let runningBillionaireList = [];
     $(`.sortable tr td`).each((i, el) => {
       if(year <= 1999) {
         return;
       }
       if(+$(el).text() <= 10) {
         if(+$(el).text() === 1) {
-          console.log('-------------------------------------------------------------------------');
-          console.log(year);
+          if(runningBillionaireList.length !== 0) {
+            topBillionairesPerYear.push({
+              [year + 1]: runningBillionaireList
+            });
+            runningBillionaireList = [];
+          }
           year--;
         }
-        console.log('rank:', $(el).text().trim());
-        console.log('name:', $(el).next().text().trim());
-        console.log('worth:', $(el).next().next().text().trim());
-        console.log('age:', $(el).next().next().next().text().trim());
-        console.log('country:', $(el).next().next().next().next().text().trim());
-        console.log('business:', $(el).next().next().next().next().next().text().trim());
-        console.log(' ');
+        runningBillionaireList.push({
+          rank: $(el).text().trim(),
+          name: $(el).next().text().trim(),
+          worth: $(el).next().next().text().trim(),
+          age: $(el).next().next().next().text().trim(),
+          country: $(el).next().next().next().next().text().trim(),
+          business: $(el).next().next().next().next().next().text().trim()
+        });
+        // console.log('running list:', runningBillionaireList);
       }
       else {
         return;
       }
     });
   }
+  // console.log('billionaires:', topBillionairesPerYear);
 });
