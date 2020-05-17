cb8404289ca190175b36a0e255de8cb5ff60a51d
AwrenNuit <a.rennaker@yahoo.com>
2020-05-16 19:52:16 -0500

    streamlined data structure

--- scraper.js
+++ scraper.js
@@ -21,7 +21,7 @@ customHeaderRequest.get(listUrl, (error, response, body) => {
       if(+$(el).text() <= 10) {
         if(+$(el).text() === 1) {
           if(runningBillionaireList.length === 10 || year + 1 === 2003 && runningBillionaireList.length === 11 || year + 1 === 2001 && runningBillionaireList.length === 11) {
-            topBillionairesPerYear[year + 1] = runningBillionaireList;
+            topBillionairesPerYear['year' + (year + 1)] = runningBillionaireList;
             runningBillionaireList = [];
           }
           year--;
@@ -38,6 +38,7 @@ customHeaderRequest.get(listUrl, (error, response, body) => {
       else {
         return;
       }
+      // console.log('running list:', runningBillionaireList);
     });
   }
   console.log('billionaires:', topBillionairesPerYear);
